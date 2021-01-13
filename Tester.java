2c7319d39d244d6a1e9183ad506e5425576c3d1a
OliverSantiago <osantiagokermani20@stuy.edu>
2021-01-12 20:42:14 -0500

    tests

---
+++ Tester.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Tester{
+  public static void main( String[]args ){
+    // System.out.println(Radix.nth(1234,0));
+    // System.out.println(Radix.nth(1234,1));
+    // System.out.println(Radix.nth(1234,2));
+    // System.out.println(Radix.nth(1234,3));
+    // System.out.println(Radix.nth(1234,4));
+    
+    
+  }
+}
